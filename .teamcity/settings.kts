b5ba42bf4340d60b07f46bf85eae8073a9960008
Shreyas <shreyas.aws.devops@gmail.com>
2025-08-13 11:34:13 +0530

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,1 +1,39 @@
 
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+version = "2025.07" // Specify the TeamCity version
+
+project {
+    buildType(HelloWorld) // Reference the build type defined below
+}
+
+object HelloWorld : BuildType({
+    id("JenkinsDemo_Build") // Unique ID for the build configuration
+    name = "Demo" // Display name in TeamCity UI
+
+    steps {
+        script {
+            name = "Echo Hello"
+            scriptContent = "echo 'Hello world!'" // Simple shell script step
+        }
+    }
+
+    triggers {
+        vcs {
+            // Trigger the build on changes in the associated VCS root
+        }
+    }
+
+    requirements {
+        // Define agent requirements, e.g., system.os = "Linux"
+    }
+
+    features {
+        // Add build features, e.g., commit status publisher
+    }
+
+    params {
+        // Define build parameters, e.g., text("myParam", "defaultValue")
+    }
+})
