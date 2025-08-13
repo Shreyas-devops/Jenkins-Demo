927b53f8ccbacca4c9e7478a0fc9a57cbf11a59f
Shreyas <shreyas.aws.devops@gmail.com>
2025-08-13 11:00:00 +0530

    Update settings.kts

--- settings.kts
+++ settings.kts
@@ -1,40 +1,32 @@
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
-object Build : BuildType({
-    name = "Build"
+import jetbrains.buildServer.configs.kotlin.v2025.07.*
+import jetbrains.buildServer.configs.kotlin.v2025.07.project
+import jetbrains.buildServer.configs.kotlin.v2025.07.buildSteps.*
 
-    vcs {
-        root(HttpsGithubComShreyasDevopsJenkinsDemoGitRefsHeadsMain)
-    }
+version = "2025.07" // Specifies the TeamCity server version
 
-    steps {
-        script {
-            name = "Build"
-            id = "Build"
-            scriptContent = """echo "this is build step""""
+project {
+    name = "Jenkins_Demo" // Name of the TeamCity project
+
+    vcsRoot(DslContext.settingsRoot) // Uses the VCS root where the settings.kts file resides
+
+    buildType {
+        name = "JenkinsDemo_Build // Name of the build configuration
+        id = RelativeId("JenkinsDemo_Build") // Unique ID for the build configuration
+
+        steps {
+            // Define a build step, e.g., running a shell script
+            script {
+                name = "Run Tests"
+                scriptContent = "echo 'Running tests...'" // Simple shell command
+            }
         }
-        script {
-            name = "Test"
-            id = "Test"
-            scriptContent = """echo "this is test step""""
-        }
-        script {
-            name = "Deploy"
-            id = "Deploy"
-            scriptContent = """echo " This  is deploy step...""""
-        }
-    }
 
-    triggers {
-        vcs {
+        triggers {
+            // Define a trigger, e.g., a VCS trigger to run on changes
+            vcs {
+                triggerRules = "" // Empty rules mean it triggers on any VCS change
+            }
         }
     }
-
-    features {
-        perfmon {
-        }
-    }
-})
+}
